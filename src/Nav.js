657e3bdcac24c345cc53a1a4140eaec331bd5a24
mia-chou <mia.fy.chou@hotmail.com>
2024-03-12 02:29:17 +0000

    Changed navbar font size

--- src/Nav.js
+++ src/Nav.js
@@ -15,17 +15,17 @@ export default function Navigation() {
           <img
             src={Logo}
             alt="Your Logo"
-            height="200" // Adjust the height as needed
+            height="80" // Adjust the height as needed
             className="d-inline-block align-top"
           />{" "}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#about">About Me</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#home"><strong>Home</strong></Nav.Link>
+            <Nav.Link href="#projects"><strong>Projects</strong></Nav.Link>
+            <Nav.Link href="#about"><strong>About Me</strong></Nav.Link>
+            <Nav.Link href="#contact"><strong>Contact</strong></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
