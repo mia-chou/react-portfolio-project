6dee3e3fb6ad73625f8521913507b251df807bb5
mia-chou <mia.fy.chou@hotmail.com>
2024-06-11 09:13:03 +0100

    Rewrite about me section

--- src/About.js
+++ src/About.js
@@ -1,25 +1,23 @@
 import React from "react";
-import Portrait from "./image of me.jpg";
 import "./About.css";
 
 export default function About() {
   return (
     <div className="About" id="about">
-      <h2 className="introduction">Hi, I'm Fang-Yi Chou, but people usually call me Mia</h2>
-      
+      <h2 className="introduction">
+        Hi, I'm Fang-Yi Chou, but people usually call me Mia
+      </h2>
+
       <div className="row">
         <div className="col-sm-6">
           <p className="about-me">
-          As a freelance Front-End developer based in West Yorkshire, Leeds, I hold a Masters degree in Communication and Media. Approximately a year ago, I began learning to code, motivated by a curiosity to understand the details of my web app projects. Proficient in HTML, CSS, JavaScript, TypeScript, React.js, Python, and responsive design, I have a strong skill set in web development. Eager to explore new software and automation technology, I excel in teamwork and welcome new and creative challenges to make a positive difference in the online world.
-          </p>
-          <p className="CV">
-            <a href="
-https://s3.amazonaws.com/shecodesio-production/uploads/files/000/120/649/original/Mia_Chou_CV.jpg?1711381405" target="_blank" rel="noreferrer" style={{ fontSize: "22px" }}>
-            </a>
+          As a freelance Front-End developer with a Master's degree in Communication and Media from the University of Leeds, I began my coding journey about a year ago. Driven by a curiosity to understand the inner workings of web apps, I have since developed skills in HTML, CSS, JavaScript, TypeScript, React.js, Python, and responsive design. I enjoy exploring new software and automation technologies, and I thrive in collaborative environments. I'm always eager to embrace creative challenges that make a positive impact in the online world.
+
+
           </p>
         </div>
         <div className="col-sm-6">
-          <img src={Portrait} className="img-fluid" alt="me" />
+          {/* Add content here if needed */}
         </div>
       </div>
     </div>
